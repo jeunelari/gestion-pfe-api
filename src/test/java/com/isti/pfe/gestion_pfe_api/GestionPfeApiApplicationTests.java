fccad042e6868a053ccd9d1c6e788a4dbe0cc58c
jeunelari <faelmilapro@gmail.com>
2024-10-13 18:52:25 +0000

    r
    Ajout fichier spring boot

---
+++ src/test/java/com/isti/pfe/gestion_pfe_api/GestionPfeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isti.pfe.gestion_pfe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPfeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
